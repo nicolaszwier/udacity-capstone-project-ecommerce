25c34b98d33b56b61be4af142afdb746d896f801
Nicolas Zwierzykowski <nicolas@softniels.com.br>
2020-06-12 23:20:02 -0300

    new environment variables in setup.sh

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -21,7 +21,7 @@ def create_app(test_config=None):
 
     @app.after_request
     def after_request(response):
-        # response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers',
                              'Content-Type,Authorization,true')
         response.headers.add('Access-Control-Allow-Methods',
--- Backend/src/setup.sh
+++ Backend/src/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export DATABASE_URL=postgres://qursypmfdclyso:dd18e075bae8d5a07c18a68b0761f92336c692c54e60cdf889cb7b85e9d80124@ec2-52-6-143-153.compute-1.amazonaws.com:5432/d8vds55g1ipovh
 export EXCITED=true
+export AUTH0_DOMAIN=nicolasdev.auth0.com
+export API_AUDIENCE=pKTGHsIqczKBuFkuknWc6LkVyfaOG0hm
 export database_name=ecommerce
 export database_path=postgresql:///ecommerce
